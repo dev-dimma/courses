1a7eb0cdde3a073c017a949c7ccf47f7b0525a50
Dev Dimma <devdimma@gmail.com>
2025-10-30 16:21:19 +0100

    Initial commit of the project with codes on values and variables practice

---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,35 @@
+//Values and variables
+let js = "amazing";
+console.log("amazing");
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "Godwin";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+//ASSIGNMENTS
+//Values and variables
+let country = "Nigeria";
+let continent = "Africa";
+let population = "Two Hundred and Thirty Two Million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
