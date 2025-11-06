a2ba258f8882404796a2b502464558edce193f61
Dev Dimma <devdimma@gmail.com>
2025-11-06 14:13:48 +0100

    Learnt about Data types like strings, boolean, undefined, null, number e.t.c

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,35 +1,59 @@
 //Values and variables
-let js = "amazing";
-console.log("amazing");
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log("amazing");
+// console.log(40 + 8 + 23 - 10);
 
-console.log("jonas");
-console.log(23);
+// console.log("jonas");
+// console.log(23);
 
-let firstName = "Godwin";
+// let firstName = "Godwin";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let jonas_matilda = "JM";
-let $function = 27;
+// let jonas_matilda = "JM";
+// let $function = 27;
 
-let person = "jonas";
-let PI = 3.1415;
+// let person = "jonas";
+// let PI = 3.1415;
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myFirstJob);
+
+////////////////////////////
+//Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "jonas");
+// console.log(typeof PI);
+// console.log(typeof 244442662677276355525636626n);
+// console.log(typeof userID);
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
 
 //ASSIGNMENTS
+////////////////////
 //Values and variables
-let country = "Nigeria";
-let continent = "Africa";
-let population = "Two Hundred and Thirty Two Million";
+// let country = "Nigeria";
+// let continent = "Africa";
+// let population = "Two Hundred and Thirty Two Million";
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
