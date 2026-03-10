11ee5aae11be89b4ceeb37e58cab5af771a90f44
Dimma Joel <devdimma@gmail.com>
2026-03-10 14:06:44 +0100

    Read a file asynchronously using the fs module

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 
-//Blocking, Synchronous Way
-//reading a file synchronously
-const textIn = fs.readFileSync("./txt/input.txt/", "utf-8");
-console.log(textIn);
+// //Blocking, Synchronous Way
+// //reading a file synchronously
+// const textIn = fs.readFileSync("./txt/input.txt/", "utf-8");
+// console.log(textIn);
 
-//creating a new file and writing to it synchronously
-const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("file written");
+// //creating a new file and writing to it synchronously
+// const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("file written");
+
+//Non-Blocking, Asynchronous Way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+
+console.log("Will read file");
