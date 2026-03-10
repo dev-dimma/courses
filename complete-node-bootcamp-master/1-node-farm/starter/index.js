15653d372defd53b5b87f7c8f1353c301923791b
Dimma Joel <devdimma@gmail.com>
2026-03-10 13:57:41 +0100

    created and wrote to a file synchronously using the fs

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -3,3 +3,8 @@ const fs = require("fs");
 //reading a file synchronously
 const textIn = fs.readFileSync("./txt/input.txt/", "utf-8");
 console.log(textIn);
+
+//creating a new file and writing to it synchronously
+const textOut = `This is what we know about the avocado: ${textIn}. \n Created on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("file written");
