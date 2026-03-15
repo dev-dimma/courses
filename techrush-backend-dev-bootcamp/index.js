ba26ff0503600c8774da7dd7dd37973d7de448ce
Dimma Joel <devdimma@gmail.com>
2026-03-15 17:53:26 +0100

    declared numeric variables

--- techrush-backend-dev-bootcamp/index.js
+++ techrush-backend-dev-bootcamp/index.js
@@ -6,3 +6,9 @@ var person = "John";
 console.log(person);
 let myName = "Dimma";
 console.log(myName); //Output: Dimma
+
+//Number: Numeric value
+let age = 30;
+const pi = 3.14;
+console.log(age); //output: 30
+console.log(age); //output: 3.14
