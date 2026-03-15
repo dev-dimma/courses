82a46abcb1f39154eb9530e2e1fa2686d6a07978
Dimma Joel <devdimma@gmail.com>
2026-03-15 17:49:04 +0100

    declared variables of string data types

--- techrush-backend-dev-bootcamp/index.js
+++ techrush-backend-dev-bootcamp/index.js
@@ -1,1 +1,8 @@
 //Data types
+//Primitive data types
+//String: a text enclosed in quotes
+//var, let, const
+var person = "John";
+console.log(person);
+let myName = "Dimma";
+console.log(myName); //Output: Dimma
