63f907439e24b9792fc5d7773c24dd26fcca6103
Dimma Joel <devdimma@gmail.com>
2026-03-18 17:05:35 +0100

    Learned how to use arithmetic operators

---
+++ techrush-backend-dev-bootcamp/operators.js
@@ -0,0 +1,15 @@
+//OPERATORS
+//Arithmetic Operators
+let x = 10;
+let y = 5;
+
+let z = x + y;
+let a = x - y;
+let b = x * y;
+let c = x / y + z - a;
+let d = x % y;
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
