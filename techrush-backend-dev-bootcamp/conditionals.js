4bbb1922db1abc925d0889afcf214fbd4fd63320
Dimma Joel <devdimma@gmail.com>
2026-03-18 18:37:37 +0100

    Conditionals lessons with if/else and else if statements

---
+++ techrush-backend-dev-bootcamp/conditionals.js
@@ -0,0 +1,19 @@
+//If/Else Statements
+let userAge = 16;
+if (userAge >= 18) {
+  console.log("Welcome to club 9ja.");
+} else {
+  console.log("Sorry, you're not allowed to enter.");
+}
+
+let gender = "female";
+
+if (gender === "female") {
+  console.log("Welcome to the ladies section");
+} else if (gender === "male") {
+  console.log("Welcome to the male section");
+} else if (gender === "other") {
+  console.log("Welcome to the other section");
+} else {
+  console.log("We don't know where you belong");
+}
