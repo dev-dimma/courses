2edb8082a37f6a0d020b4e8743bf9e960e1672e3
Dimma Joel <devdimma@gmail.com>
2026-03-18 14:38:30 +0100

    Recap of what was taught last class and practice of primitives and non-primitives data types

---
+++ techrush-backend-dev-bootcamp/variables.js
@@ -0,0 +1,45 @@
+var myName = "Esther";
+// let myAge = 30;
+const myHeight = 5.4;
+
+//Var - use it when the value can change and you want to reassign it later in the code
+// var myName = "John";
+// console.log(myName);
+
+// let myAge = 25;
+// console.log(myAge);
+
+// const myHeight = 5.6;
+// console.log(myHeight);
+
+//Variable Assignment
+// myName = "Adina";
+// console.log(myName);
+
+// myAge = 31;
+// console.log(myAge);
+
+// myHeight = 5.5;
+// console.log(myHeight);
+
+//DATATYPES
+//Primitives Data Types: String, Number, Boolean, Null, Undefined, Symbols
+
+let myString = "Hello World";
+let myNumber = 42;
+let myBoolean = true;
+console.log(myString);
+console.log(myNumber);
+console.log(myBoolean);
+
+//Non-Primitives Data Types: Onjects, Array, Function
+let myObject = { name: "John", age: 30 };
+let myArray = [
+  "Rice",
+  "Beans",
+  "Yam",
+  2,
+  true,
+  (30)[(John, Jane)],
+  { name: Jane, age: 25 },
+];
