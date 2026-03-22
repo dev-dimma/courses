d7202b1462efda7bc83f4ae35ff3e55225315fd8
Dimma Joel <devdimma@gmail.com>
2026-03-22 16:01:12 +0100

    Learnt about while loops

--- techrush-backend-dev-bootcamp/conditionals.js
+++ techrush-backend-dev-bootcamp/conditionals.js
@@ -1,4 +1,4 @@
-//If/Else Statements
+//IF/ELSE STATEMENTS
 let userAge = 16;
 if (userAge >= 18) {
   console.log("Welcome to club 9ja.");
@@ -16,4 +16,25 @@ if (gender === "female") {
   console.log("Welcome to the other section");
 } else {
   console.log("We don't know where you belong");
+}
+
+//real world usage of if/else statement
+let username = "Esther";
+let password = "Password123";
+
+if (username === "Esther" && password === "Password123") {
+  console.log("Login successful");
+} else if (username === "Esther" || password === "Password123") {
+  console.log("Username is correct");
+} else {
+  console.log("Login failed");
+}
+
+//Classwork
+
+let age = 17;
+if (age >= 18) {
+  console.log("You CAN vote in the election");
+} else {
+  console.log("You CAN'T vote in the election");
 }
---
+++ techrush-backend-dev-bootcamp/while-loops.js
@@ -0,0 +1,15 @@
+let tries = 0;
+while (tries < 3) {
+  console.log(tries);
+  tries++;
+}
+
+let count = 10;
+while (count > 0) {
+  console.log(count);
+  count--;
+}
+
+console.log("done");
+
+//Assignment: Give a Well detailed explanation of the differences between the while and for loop with code examples
