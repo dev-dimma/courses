4a102c8754120ac4c215489bfbc3bd1a026bd056
Dimma Joel <devdimma@gmail.com>
2026-07-06 06:59:27 +0100

    Learnt values and variables

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,27 @@
+//Values and Variables
 let js = "Amazing";
-if (js === "Amazing") alert("Javascript is fun");
-
-40 + 8 + 23 + 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// let firstName = "Matilda";
+let firstName = "Praise";
+
+console.log("firstName");
+console.log("firstName");
+console.log("firstName");
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "Jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentjob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
