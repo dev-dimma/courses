e15e5aa8d68776350e4abab129de1974a8b0abf6
Dimma Joel <devdimma@gmail.com>
2026-07-27 10:58:37 +0100

    Learnt and practiced data types

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,4 +1,5 @@
-//VALUES AND VARIABLES
+/////////////////////////////////////////////////////////////////////
+// VALUES AND VARIABLES
 // 1. Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 // 2. Log their values to the console.
 const country = "Nigeria";
@@ -8,3 +9,14 @@ const population = 242;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+/////////////////////////////////////////////////////////////////////
+//DATA TYPES
+// 1. Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+// 2. Log the types of isIsland, population, country and language to the console.
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,27 +1,53 @@
-//Values and variables
-let js = "Amazing";
-console.log(40 + 8 + 23 - 10);
+///////////////////////////////////////////////////
+// VALUES AND VARIABLES
+// let js = "Amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-// let firstName = "Matilda";
-let firstName = "Praise";
+// // let firstName = "Matilda";
+// let firstName = "Praise";
 
-console.log("firstName");
-console.log("firstName");
-console.log("firstName");
+// console.log("firstName");
+// console.log("firstName");
+// console.log("firstName");
 
-let jonas_matilda = "JM";
-let $function = 27;
+// let jonas_matilda = "JM";
+// let $function = 27;
 
-let person = "Jonas";
-let PI = 3.1415;
+// let person = "Jonas";
+// let PI = 3.1415;
 
-let myFirstJob = "Programmer";
-let myCurrentjob = "Teacher";
+// let myFirstJob = "Programmer";
+// let myCurrentjob = "Teacher";
 
-let job1 = "programmer";
-let job2 = "teacher";
+// let job1 = "programmer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+///////////////////////////////////////////////////
+//DATA TYPES
+//Boolean and string
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+//Undefined and number
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+//NulL
+console.log(typeof null); //should return nul but returned object so it's a bug in js but never corrected for legacy reasons
