7696bd3b08e774dc1f3696db9a9c96200747199c
Dimma Joel <devdimma@gmail.com>
2026-08-16 03:26:17 +0100

    Read and wrote into files synchronously using the fs module

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,5 +1,14 @@
 const fs = require("fs");
-const http = require("http");
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}. Created on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written successfully");
+
+// const fs = require("fs");
+// const http = require("http");
 
 ///////////////////////////////////
 //FILES
